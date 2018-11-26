38f67a521edd65175e464d03e68b90833f4700a7
crcms <crcms@crcms.cn>
2018-11-26 16:18:23 +0800

    tips

--- src/Facades/Service.php
+++ src/Facades/Service.php
@@ -4,9 +4,14 @@ namespace CrCms\Microservice\Client\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use CrCms\Microservice\Client\Service as ServiceInstance;
+use GuzzleHttp\Promise\Promise;
 
 /**
- * @method static object call(string $name, ?string $uri = null, array $params = [])
+ * @method static object call(string $service, $uri = '', array $params = [])
+ * @method static Promise callAsync(string $service, $uri = '', array $params = [])
+ * @method static bool status()
+ * @method static mixed getContent()
+ * @method static int getStatusCode()
  * @method static ServiceInstance driver(?string $driver = null)
  * @method static ServiceInstance connection(?string $name = null)
  *
